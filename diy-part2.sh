b63b941b0eebd5435a506456a2a35d3dd1bcc94c
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-12-01 10:11:16 +0800

    op

--- diy-part2.sh
+++ diy-part2.sh
@@ -53,17 +53,17 @@ sed -i 's/192.168.1.1/10.10.10.89/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/destan19/OpenAppFilter.git  package/lean/luci-app-oaf
 
 #add luci-app-diskman
-git clone --depth=1 https://github.com/lisaac/luci-app-diskman
-mkdir parted
-cp luci-app-diskman/Parted.Makefile parted/Makefile
+#git clone --depth=1 https://github.com/lisaac/luci-app-diskman
+#mkdir parted
+#cp luci-app-diskman/Parted.Makefile parted/Makefile
 
 #add luci-app-dockerman
 rm -rf ../lean/luci-app-docker
 #git clone --depth=1 https://github.com/lisaac/luci-app-dockerman
 #git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 
-git clone https://github.com/lisaac/luci-in-docker.git package/luci-in-docker
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+#git clone https://github.com/lisaac/luci-in-docker.git package/luci-in-docker
+#git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 
 #add dnscrypt-proxy2
 
