9164aa2db34b3f4331382ada395cf88caeed01d4
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-12-14 10:03:04 +0800

    p

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ sed -i 's/OpenWrt/William build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/de
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Add kernel build user
 [ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
@@ -39,7 +39,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/
 
 
 # 删除默认argon主题，并下载新argon主题
-#rm -rf package/lean/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
